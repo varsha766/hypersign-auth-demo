6a6f8d79e2e8a5630475e48ab64171dc512d7591
varsha766 <varshakumari370@gmail.com>
2021-11-29 05:25:33 +0530

    implemented registration flow with polling

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## Hypersign autheentication using websocket
+## Hypersign authentication using polling for registration flow
+
+* In this user has to register 
+* After registration  he/she can login to the page and get access to the resource
+
+
--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const HypersignAuth = require('hypersign-auth-js-sdk');
 
 const app = express();
 const server = http.createServer(app);
-const port = 5020;
+const port = 4000;
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json())
 const hypersign = new HypersignAuth(server);
+
+
+
+app.post("/challenge", hypersign.challenge.bind(hypersign), (req, res) => {
+  res.status(200).send(req.body);
+});
+
+app.get("/poll", hypersign.poll.bind(hypersign), (req, res) => {
+  res.status(200).send(req.body);
+});
 app.post("/hs/api/v2/auth", hypersign.authenticate.bind(hypersign), async (req, res) => {
   try {
     const user = req.body.hsUserData;
@@ -31,7 +41,8 @@ app.get('/register', (req, res) => {
 // for registration
 
 // Implement /register API: 
-//Analogous to register user but not yet activate
+// Analogous to register user but not yet activate
+ 
 app.post('/hs/api/v2/register', hypersign.register.bind(hypersign), (req, res) => {
   try {
 
@@ -45,12 +56,14 @@ app.post('/hs/api/v2/register', hypersign.register.bind(hypersign), (req, res) =
   }
 })
 
+
 // Implement /credential API: 
-//Analogous to active user
-app.get('/hs/api/v2/credential', hypersign.issueCredential.bind(hypersign), (req, res) => {
+// Analogous to active user
+
+
+app.get('/auth/credential', hypersign.issueCredential.bind(hypersign), (req, res) => {
   try {
-    console.log(4)
-    console.log('Credential success');
+       console.log('Credential success');
     res.status(200).send({ status: 200, message: req.body.verifiableCredential, error: null });
 
   } catch (e) {
@@ -64,15 +77,18 @@ app.get('/', (req, res) => {
   res.sendFile("./public/login.html", { root: __dirname })
 })
 
-app.get('/home', (req, res) => {
-  res.sendFile("./public/home.html", { root: __dirname })
-})
-app.get('/protected', hypersign.authorize.bind(hypersign), (req, res) => {
-  res.send({ message: 'this is a get api' })
-})
-app.post('/protected', hypersign.authorize.bind(hypersign), (req, res) => {
-  res.send({ message: 'this is a post api' })
-})
+
+app.post("/protected", hypersign.authorize.bind(hypersign), (req, res) => {
+  try {
+    const user = req.body.hypersign.data;
+    console.log(user);
+    // Do whatever you want to do with it
+    res.status(200).send({ status: 200, message: user, error: null });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
 server.listen(port, () => {
 
   console.log('server is running on port', port);
